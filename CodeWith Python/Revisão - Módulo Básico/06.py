e78056f22b7882d1af90a58f1d7e92608697066c
Eng-LeonardoH <leonardoham321@gmail.com>
2023-10-01 02:04:40 -0300

    Ajuste dos nomes dos arquivos.

---
+++ CodeWith Python/Revisão - Módulo Básico/06.py
@@ -0,0 +1,11 @@
+# 6. Ler um valor e escrever a mensagem É MAIOR
+# QUE 10! se o valor lido for maior que 10, caso
+# contrário escrever NÃO É MAIOR QUE 10!
+
+numero = int(input("Digite um número: "))
+while numero == 10:
+    numero = float(input("Número não pode ser dez... Digite um número: "))
+if numero > 10:
+    print("É MAIOR QUE 10!")
+else:
+    print("NÃO É MAIOR QUE 10")
---
+++ CodeWith Python/Revisão - Módulo Básico/09.py
@@ -0,0 +1,23 @@
+# 9. Faça um código que receba um número inteiro e mostre o seu antecessor.
+numero = 0
+opcao_menu = "0"
+while opcao_menu != "3":
+    opcao_menu = input("""------------------------------------------------------------------
+    Opções: 1 para ver o sucessor. 
+            2 para ver o antecessor.
+            3 para encerrar o programa.
+    Digite: """)
+    print("------------------------------------------------------------------")
+
+    if opcao_menu == "1":
+        numero = int(input("Digite um número inteiro: "))
+        print("------------------------------------------------------------------")
+        numero += 1
+        print(f"O sucessor é {numero}.")
+    elif opcao_menu == "2":
+        numero = int(input("Digite um número inteiro: "))
+        print("------------------------------------------------------------------")
+        numero -= 1
+        print(f"O antecessor é {numero}.")
+    elif opcao_menu == "3":
+        print("Fim do programa!")
