3f6dd83bbc3a37a34ce2d251aa0a4615ae75551a
Eng-LeonardoH <leonardoham321@gmail.com>
2023-10-03 11:41:22 -0300

    Update - Atividades da aula do dia 03/10/23.

--- CodeWith Python/Desafio 1.py
+++ CodeWith Python/Desafio 1.py
@@ -1,17 +1,25 @@
 jogadores = ["",""]
+score = ["",""]
 opcoes = ["Pedra", "Papel", "Tesoura"]
 entrada01 = 0
 entrada02 = 0
-fim_partida = 0
+fim_partida = False
+turno = 0
 
-for i in range (2):
-    jogadores[i] = input(f"Digite o nome do {i+1}º jogador: ")
+jogadores[0] = "Leo" #input(f"Digite o nome do 1º jogador: ") TESTES
+jogadores[1] = "Juca" #input(f"Digite o nome do 2º jogador: ") TESTES
 
-print("Jogador 1 inicia a partida...")
-while fim_partida != 1:
-    entrada01 = int(input())
-Jogador 1, escolha uma opção...
-1 para Pedra
-2 para Papel
-3 para Tesoura
-""")
+#for i in range(2):
+#   jogadores[i] = input(f"Digite o nome do {i}º jogador: ")
+while fim_partida == False:
+    for turno in range(0, 2):
+        entrada01 = int(input(f"""{jogadores[0]}, escolha uma opção...\n1 para Pedra\n2 para Papel\n3 para Tesoura"""))
+        while entrada01 < 1 or entrada01 > 3:
+            entrada01 = int(input("Erro na entrada... Jogador 1, escolha uma opção...\n1 para Pedra\n2 para Papel\n3 para Tesoura"))
+        entrada02 = int(input("Jogador 2, escolha uma opção...\n1 para Pedra\n2 para Papel\n3 para Tesoura"))
+        while entrada02 <1 or entrada02 > 3:
+            entrada02 = int(input("""Erro na entrada... Jogador 2, escolha uma opção...\n1 para Pedra\n2 para Papel\n3 para Tesoura"""))
+        if entrada01 == 1:
+            if entrada02 == 3:
+                print("Jogador 1 venceu!")
+                score[0] +=1
